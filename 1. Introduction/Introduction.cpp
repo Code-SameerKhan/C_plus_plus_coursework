d9dc7c896581b48260edd0e20b145f5d2cc56563
Sameer Khan <sameer567khan@gmail.com>
2022-01-20 18:02:45 +0530

    Added files

---
+++ 1. Introduction/Introduction.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+
+int main() { // Main function
+	cout << "This is introductory C++ file!" << endl;
+	return 0; // A Statement
+}
--- Introduction/Introduction.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-
-using namespace std;
-
-int main() { // Main function
-	cout << "This is introductory C++ file!" << endl;
-	return 0; // A Statement
-}
