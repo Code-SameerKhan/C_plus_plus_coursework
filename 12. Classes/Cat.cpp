587fea44c03a385badf281b24d633a215152ed49
Sameer Khan <sameer567khan@gmail.com>
2022-02-15 16:23:01 +0530

    Updates

---
+++ 12. Classes/Cat.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include "Cat.h"
+
+using namespace std;
+
+void Cat::speak() {
+	cout << "Meowww <3" << endl;
+}
+
+void Cat::jump() {
+	cout << "The cat jumped!" << endl;
+}
---
+++ 12. Classes/Cat.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef CAT_H_
+#define CAT_H
+
+class Cat {
+public:
+	void speak();
+	void jump();
+};
+
+
+#endif /*CAT_H_*/ 
