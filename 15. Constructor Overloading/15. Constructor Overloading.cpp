b8223ec6d39908f400b360e0518cb75314af8a74
Sameer Khan <sameer567khan@gmail.com>
2022-03-10 20:58:40 +0530

    Update 15. Constructor Overloading.cpp

--- 15. Constructor Overloading/15. Constructor Overloading.cpp
+++ 15. Constructor Overloading/15. Constructor Overloading.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 int main() {
-	Office employee1("Michael");
+	Office employee1("Michael");		// Constructor called chosen the one with 1 user defined parameter
 	cout << employee1.toString() << endl;
 	
 	Office employee2("Jim",29);
 	cout << employee2.toString() << endl;
 	{
 		cout << "\tAdding record" << endl;
-		Office employee3;
+		Office employee3;		// Default constructor with no defined parameters
 		cout << employee3.toString() << endl;
 	}
 	return 0;
