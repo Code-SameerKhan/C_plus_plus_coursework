6340aab9ce24e368cd500edd113c35d13ab9e5f6
Sameer Khan <sameer567khan@gmail.com>
2022-03-12 21:39:00 +0530

    Updates

--- 18. Pointers and Arrays/18. Pointers and Arrays.cpp
+++ 18. Pointers and Arrays/18. Pointers and Arrays.cpp
@@ -4,7 +4,37 @@ using namespace std;
 
 int main() {
 
+	string texts[] = { "Duck","Chicken","Rooster" }; // Array
 
+	int length = sizeof(texts) / sizeof(string);
+	cout << length << endl;
+	string *pTexts = texts; // Pointer towards starting of array
 
+	for (int i = 0; i < length; i++) // Looping through array index
+	{
+		cout << pTexts[i] << " " << flush;
+	}
+	cout << endl;
+
+	for (int i = 0; i < length; i++, pTexts++) // Looping through array pointer and increment
+	{
+		cout << *pTexts << " " << flush;
+	}
+	cout << endl;
+
+	string* pElement = texts;  // Pointer for the starting element of array
+	string* pEnd = &texts[2]; // Pointer for the end element
+
+	while (true)
+	{
+		cout << *pElement << " " << flush;
+
+		if (pElement == pEnd)	// Checking if current element is same as the end element
+		{
+			break;
+		}
+
+		pElement++;
+	}
 	return 0;
 }
